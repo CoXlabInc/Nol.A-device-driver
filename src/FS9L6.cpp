3c3ca2340f498720d6863594cd8aae8b2357ad32
hogi <hogi1228@gmail.com>
2018-08-13 16:33:34 +0900

    no message

--- src/FS9L6.cpp
+++ src/FS9L6.cpp
@@ -2,74 +2,54 @@
 #include "cox.h"
 
 FS9L6::FS9L6(){
-  printf("********* Start *********\n");
-  printf("Check and maintain the current. \n");
+  printf("*********** FS9L6 ************\n");
 }
 
-void FS9L6::begin(){
-  this->VoltageValue = 0;
-
-  printf("Find Standard Current...\n");
-  for (int i=0 ; i<3000; i++) {
-    this->DataValue = analogRead(A0);
-    if (this->DataValue > this->VoltageValue) this->VoltageValue = this->DataValue;
-  }
-
-  this->StandardVoltage = this->VoltageValue / 1.414 / 4095.0 * 2.5; // when the voltage 2.5, it is 4095.0 on the Nol.Board and rms is Vp / sqr(2)
-  this->StandardCurrent = this->StandardVoltage / 330 * 600; // Resister is 330ohms and FS9L6s winding ratio is 600 : 1
-
-  printf("Current is ");
-  Serial.print(this->StandardCurrent, 3);
-  printf("A.\n");
-
-  // Ref voltage
-  printf("Voltage is ");
-  Serial.print(this->StandardVoltage, 3);
-  printf("Vrms.\n");
-
-  this->StandardMaxCurrent = this->StandardCurrent * (1+tolerance);
-  this->StandardMinCurrent = this->StandardCurrent * (1-tolerance); // safety margin
-  this->Current = this->StandardCurrent;
-
+// Save VpData
+void FS9L6::VpData(){
+  VpDataH = this->voltageRMS;
+  VpDataL = this->voltageRMS * 1000;
 }
+// !Save VpData
 
-void FS9L6::CurrentChange(){
-  printf("Current is change.\n");
-  printf("Apply New StandardCurrent\n");
-  this->begin();
+// Save CurrentData
+void FS9L6::CurrentData(){
+  CurrentDataH = this->Current;
+  CurrentDataL = this->Current * 1000;
 }
+// !Save CurrentData
 
+// Measure and Caculate AC
 void FS9L6::Maintain(){
+    this->VoltageValue = 0;
 
-  while(this->StandardMinCurrent < this->Current && this->Current < this->StandardMaxCurrent)
-  {
-    maxData = 0;
-
-    for (int i=0 ; i<dataNum ; i++) {
+    for (int i=0 ; i<dataNum; i++) {
       this->DataValue = analogRead(A0);
-      if (this->DataValue > this->maxData) this->maxData = this->DataValue;
+      if (this->DataValue > this->VoltageValue) this->VoltageValue = this->DataValue;
     }
-    this->voltageRMS = this->maxData / 1.414  / 4095.0 * 2.5;
-    this->voltageP = this->maxData / 4095.0 * 2.5;
-    this->Current = this->maxData / 1.414  / 4095.0 / 330 * 2.5 * 600;
+    //calculate data
+    this->voltagePeak = this->VoltageValue / 4095.0 * 2.5;
+    this->voltageRMS = this->VoltageValue / 1.414 / 4095.0 * 2.5;
+    this->Current = this->VoltageValue / 1.414 / 4095.0 / 330 * 2.5 * 600;
 
+    //show data (serial port)
+    /*
     printf("Standard Current = ");
     Serial.print(this->StandardCurrent, 3);
     printf(".\n");
 
     Serial.print("* Vp = ");
-    Serial.print(this->voltageP, 3);
+    Serial.print(this->voltagePeak, 3);
     Serial.print(", * Vrms = ");
     Serial.print(this->voltageRMS, 3);
     Serial.print(", * Current = ");
     Serial.print(this->Current, 3);
     printf("\n");
+    */
+    //!show data (serial port)
 
-    if(this->Current > this->StandardMaxCurrent || this->StandardMinCurrent > this->Current ){
-      this->CurrentChange();
-    }
-
-  }
-
+    this->VpData();
+    this->CurrentData();
 
 }
+// !Measure and Caculate AC

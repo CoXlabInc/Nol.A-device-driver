98a1fb92490f7500344177e74e40c01e38883fb1
Jongsoo Jeong <jsjeong@coxlab.kr>
2019-04-20 16:36:28 +0900

    Added license text.

--- inc/Adafruit_AMG88xx.hpp
+++ inc/Adafruit_AMG88xx.hpp
@@ -1,3 +1,20 @@
+/*
+# Adafruit AMG88xx Library
+
+<img src="https://cdn-shop.adafruit.com/970x728/3538-01.jpg" height="300"/>
+
+This is a library for the Adafruit AMG88xx based thermal cameras:
+  * https://www.adafruit.com/products/3538
+  * https://www.adafruit.com/product/3622
+ 
+Check out the links above for our tutorials and wiring diagrams. This chip uses I2C to communicate
+
+Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+
+Written by Dean Miller for Adafruit Industries.
+MIT license, all text above must be included in any redistribution
+*/
+
 #pragma once
 
 #include <nola-common.h>
